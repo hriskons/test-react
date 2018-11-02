bf0b1edac1a322fd8a98c5508efca419ba1e1240
Konstantas <christos.konstantas@accenture.com>
2018-11-02 20:49:57 +0200

    Updates

---
+++ precache-manifest.52ee76d5c66d5738144d2dc5193294f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "056c8d9a730cc8cd4a6f",
+    "url": "/test-react/static/js/runtime~main.056c8d9a.js"
+  },
+  {
+    "revision": "fcfe669d440aaa329ef9",
+    "url": "/test-react/static/js/main.fcfe669d.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/test-react/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "fcfe669d440aaa329ef9",
+    "url": "/test-react/static/css/main.d98cee9e.chunk.css"
+  },
+  {
+    "revision": "187a1c1853f20cfc788d4e9d3f89e8e3",
+    "url": "/test-react/index.html"
+  }
+];
